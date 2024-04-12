e5bfc37165f71439d01fe56e8727da01866117e4
storylei <storylei@126.com>
2024-04-12 17:35:13 +0200

    Implement and test listQuestions function

--- week04/qa-server/dao.mjs
+++ week04/qa-server/dao.mjs
@@ -13,91 +13,97 @@ const db = new sqlite.Database('questions.sqlite', (err) => {
 // get all the questions
 export const listQuestions = () => {
   // write something clever
-}
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * from question';
+    db.all(sql, (err, rows) => {
+      if (err) reject(err);
+      else if (rows === undefined) resolve({ error: 'No Questions!' });
+      else {
+        console.log(rows);
+        let res = [];
+        rows.forEach((row) => {
+          res.push(new Question(row.id, row.text, row.email, row.date));
+        });
+        resolve(res);
+      }
+    });
+  });
+};
 
 // get a question given its id
 export const getQuestion = (id) => {
   return new Promise((resolve, reject) => {
-    const sql = 'SELECT question.*, user.email FROM question JOIN user ON question.authorId = user.id WHERE question.id = ?';
+    const sql =
+      'SELECT question.*, user.email FROM question JOIN user ON question.authorId = user.id WHERE question.id = ?';
     db.get(sql, [id], (err, row) => {
-      if (err)
-        reject(err);
-      else if (row === undefined)
-        resolve({error: "Question not available, check the inserted id."});
+      if (err) reject(err);
+      else if (row === undefined) resolve({ error: 'Question not available, check the inserted id.' });
       else {
         resolve(new Question(row.id, row.text, row.email, row.date));
       }
     });
   });
-}
+};
 
 // add a new question
 export const addQuestion = (question) => {
   return new Promise((resolve, reject) => {
     let sql = 'SELECT id from user WHERE email = ?';
     db.get(sql, [question.email], (err, row) => {
-      if (err)
-        reject(err);
-      else if (row === undefined)
-        resolve({error: "Author not available, check the inserted email."});
+      if (err) reject(err);
+      else if (row === undefined) resolve({ error: 'Author not available, check the inserted email.' });
       else {
         sql = 'INSERT INTO question(text, authorId, date) VALUES(?,?,DATE(?))';
         db.run(sql, [question.text, row.id, question.date.toISOString()], function (err) {
-          if (err)
-            reject(err);
-          else
-            resolve(this.lastID);
+          if (err) reject(err);
+          else resolve(this.lastID);
         });
       }
     });
   });
-}
+};
 
 /** ANSWERS **/
 
 // get all the answer of a given question
 export const listAnswersOf = (questionId) => {
   return new Promise((resolve, reject) => {
-    const sql = 'SELECT answer.*, user.email FROM answer JOIN user ON answer.authorId=user.id WHERE answer.questionId = ?';
+    const sql =
+      'SELECT answer.*, user.email FROM answer JOIN user ON answer.authorId=user.id WHERE answer.questionId = ?';
     db.all(sql, [this.id], (err, rows) => {
-      if (err)
-        reject(err)
+      if (err) reject(err);
       else {
         const answers = rows.map((ans) => new Answer(ans.id, ans.text, ans.email, ans.date, ans.score));
         resolve(answers);
       }
     });
   });
-}
+};
 
 // add a new answer
 export const addAnswer = (answer) => {
   return new Promise((resolve, reject) => {
     let sql = 'SELECT id from user WHERE email = ?';
     db.get(sql, [answer.email], (err, row) => {
-      if (err)
-        reject(err);
-      else if (row === undefined)
-        resolve({error: "Author not available, check the inserted email."});
+      if (err) reject(err);
+      else if (row === undefined) resolve({ error: 'Author not available, check the inserted email.' });
       else {
-        sql = "INSERT INTO answer(text, authorId, date, score, questionId) VALUES (?, ?, DATE(?), ?, ?)";
+        sql = 'INSERT INTO answer(text, authorId, date, score, questionId) VALUES (?, ?, DATE(?), ?, ?)';
         db.run(sql, [answer.text, row.id, answer.date.toISOString(), answer.score, this.id], function (err) {
-          if (err)
-            reject(err);
-          else
-            resolve(this.lastID);
+          if (err) reject(err);
+          else resolve(this.lastID);
         });
       }
     });
   });
-}
+};
 
 // update an existing answer
 export const updateAnswer = (answer) => {
   // write something clever
-}
+};
 
 // vote for an answer
 export const voteAnswer = (answerId, vote) => {
   // write something clever
-}
+};
--- week04/qa-server/server.mjs
+++ week04/qa-server/server.mjs
@@ -1,21 +1,34 @@
-import express from 'express'
-import morgan from 'morgan'
-import { getQuestion } from './dao.mjs'
+import express from 'express';
+import morgan from 'morgan';
+import { getQuestion, listQuestions } from './dao.mjs';
 // import dao from './dao.mjs'
 //     dao.getQuestion
 
-const app = express()
-app.use(morgan('common'))
-app.use(express.json())
+const app = express();
+app.use(morgan('common'));
+app.use(express.json());
 
-app.get('/questions/:id', (req, res)=>{
-    const questionId = req.params.id
-    getQuestion(questionId).then((q)=>{
-        res.json(q)
-    }).catch((err)=>{
-        res.statusCode(500).send("Database error: " + err)
-    }
-    )
-})
+app.get('/questions', (req, res) => {
+  listQuestions()
+    .then((l) => {
+      res.json(l);
+    })
+    .catch((err) => {
+      res.statusCode(500).send('Database error: ' + err);
+    });
+});
 
-app.listen(3000, ()=>{console.log("Running!")})
+app.get('/questions/:id', (req, res) => {
+  const questionId = req.params.id;
+  getQuestion(questionId)
+    .then((q) => {
+      res.json(q);
+    })
+    .catch((err) => {
+      res.statusCode(500).send('Database error: ' + err);
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Running!');
+});
